1cdbee0cdcdc82d553ea71f1ca59521a3967247f
Teerachaya <teerachaya@192.168.1.103>
2024-10-21 04:39:30 +0700

    Update new for render

--- alz_system/settings.py
+++ alz_system/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'app_general/static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
